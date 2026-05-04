c1203647acb0026cde91889b1abcfe367b4c7d65
AngelinaVB <b.lina.1995@mail.ru>
2026-05-04 20:02:55 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import Controller from "./Controller";
+import Collapsible from "./Collapsible";
+
+const container = document.querySelector(".container");
+const collapsible = new Collapsible(container);
+
+const controller = new Controller(collapsible);
+controller.init();
